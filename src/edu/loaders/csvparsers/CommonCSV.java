e55cb591003be7f849eb3004b47c4daa87d0902b
Christian Apostoli <54412557+cpa4001@users.noreply.github.com>
2020-11-30 14:18:26 -0500

    Added some javadocs to CommonCSV.java
    Added javadocs to already built methods in CommonCSV.java.
    Since other methods were not built no javadocs were added

--- src/edu/loaders/csvparsers/CommonCSV.java
+++ src/edu/loaders/csvparsers/CommonCSV.java
@@ -6,15 +6,24 @@ import java.util.List;
 
 public abstract class CommonCSV {
   private String delimiter;
-
+  
+  /** Sets the delimiter to a comma for
+  *   comma seperated values
+  */
   public CommonCSV() {
     this.delimiter = ",";
   }
-
+  
+  /** Sets the delimiter to the passed string 
+  *   @param delimiter the String that will be used as a delimiter
+  */
   public CommonCSV(String delimiter) {
     this.delimiter = delimiter;
   }
-
+  
+  /** Returns the delimiter
+  *   @return returns the delimiter as a String 
+  */
   public String getDelimiter() {
     return delimiter;
   }
