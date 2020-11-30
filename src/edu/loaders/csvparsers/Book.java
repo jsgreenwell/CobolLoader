d38c83a6f9ccdd64087e1877c8fa5baee62cad4d
Christian Apostoli <54412557+cpa4001@users.noreply.github.com>
2020-11-30 14:03:40 -0500

    Added javadocs to Book.java

--- src/edu/loaders/csvparsers/Book.java
+++ src/edu/loaders/csvparsers/Book.java
@@ -4,7 +4,9 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
+
 public class Book {
+  // Uses unknown or none if the title, author, or editor is blank or not given
   protected long isbn = 0;
   protected String title = "unknown";
   protected String author = "unknown";
@@ -17,6 +19,17 @@ public class Book {
 
   private int internalPrice = 0;
 
+ /** Constructor to create book objects with all arguments
+ *   and calls the setGenre and setPrice methods to set 
+ *   the genre and internal price.
+ *   @param isbn the isbn number of the book
+ *   @param title the title of the book
+ *   @param author the first and last name of the author of the book
+ *   @param pages the number of pages the book has
+ *   @param eidtor the first and last name of the editor of the book
+ *   @param genre the genre of the book
+ *   @param price the price of the book in dollars and cents
+ */
   public Book(long isbn, String title, String author, int pages, String editor,
       String genre, double price) {
     this.isbn = isbn;
@@ -27,7 +40,13 @@ public class Book {
     setGenre(genre);
     setPrice(price);
   }
-
+  
+` /** Constructs a book object using a passed string array and
+  *   sets each element in array to a clas variable and
+  *   calls the setGenre mathod and setPrice method
+  *   @param csvLine the comma seperated value which contains
+  *   the isbn, title, author, pages, editor
+  */
   public Book(String[] csvLine) {
     isbn = Long.parseLong(csvLine[0]);
     title = csvLine[1].substring(1, csvLine[1].length()-1);
@@ -38,24 +57,43 @@ public class Book {
     setPrice(Double.parseDouble(csvLine[6]));
   }
 
+  /** Constructor to create Book object with no given arguments
+  */
   public Book() {
 
   }
-
+  
+  /** Returns the stored author of the Book object
+  *   @return the first and last name of the author of the book
+  */
   public String getAuthor() { return author; }
-
+  
+  /** Returns the price of the book in cents
+  *   @return the price of the book in cents
+  */
   public int getInternalPrice() {
     return internalPrice;
   }
-
+  
+  /** Returns the price of the book as a double (dollars and cents) 
+  *   @return the price of the book in dollars and cents
+  */
   public double getPrice() {
     return internalPrice / 100.0;
   }
-
+  
+  /** Sets the internal price to the passed price of the book
+  *   in cents
+  *   @param price the price of the book in dollars and cents
+  */
   public void setPrice(double price) {
     this.internalPrice = (int) (price * 100);
   }
-
+  
+  /** Sets the passed genre to one of the genres of the 
+  *   GENRES set or "other" otherwise
+  *   @param genre the genre of the book as a String
+  */
   public void setGenre(String genre) {
     if (GENRES.contains(genre)) {
       this.genre = genre;
@@ -63,7 +101,11 @@ public class Book {
       this.genre = "other";
     }
   }
-
+  
+  /** Returns all the class variables and the price
+  *   of the book
+  *   @return the isbn, title, author, pages, editor, genre, and price of the book as a string
+  */
   @Override
   public String toString() {
     return isbn + ",'" + title + "','"
@@ -71,6 +113,9 @@ public class Book {
         + ",'" + editor + "','"
         + genre + "'," + getPrice() + "\n";
   }
-
+  
+  /** Returns the title of the book
+  *   @return the title of the book as a string
+  */
   public String getTitle() { return title;  }
 }
