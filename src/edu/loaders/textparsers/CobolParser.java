b1744eaf9186c84641ab4eaea9a1eafb53b3deb3
GovindaJR <62119685+GovindaJR@users.noreply.github.com>
2020-11-30 21:46:44 -0500

    fixed grammar mistakes in CobolParser.java
    Fixed comment issues as requested in issue #7

--- src/edu/loaders/textparsers/CobolParser.java
+++ src/edu/loaders/textparsers/CobolParser.java
@@ -12,7 +12,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class CobolParser {
-  // a property called filename which is set in constructor
+  // A property called "filename" which is set in constructor.
   private Path cobFile;
   private List<String> parsedLines = new ArrayList<>();
 
@@ -27,33 +27,33 @@ public class CobolParser {
   }
 
   protected void loadFile() throws IOException {
-    // You can use a BufferedReader here but to show the Stream way:
+    // You can use a BufferedReader here, but to show the Stream way:
     Stream<String> stream = Files.lines(cobFile);
     parsedLines = stream
-        .filter(l -> !l.equals("")) // ignore blank lines
-        .filter(l -> !l.startsWith("*")) // ignore comments
-        .map(String::toLowerCase) // make everything lowercase
+        .filter(l -> !l.equals("")) // Ignore blank lines.
+        .filter(l -> !l.startsWith("*")) // Ignore comments.
+        .map(String::toLowerCase) // Make everything lowercase.
         .collect(Collectors.toList());
   }
 
   public String getDataFileNoRegex() {
-    /* old way with no regex
-        Problem with this way is what if there are only 2 spaces (you can use \\s+ with regex)
-        What if the lowercase wasn't done (if someone inhereted this and overloaded)
-        we cannot use contains because we need an exact match
-          remember I ran this over many files so lots of audit.dat, audit.1783783.dat, rev.8371.dat
+    /* Old way with no regex.
+        Problem with this way is, what if there are only 2 spaces? (You can use \\s+ with regex.)
+        What if the lowercase wasn't done? (If someone inhereted this and overloaded.)
+        We cannot use contains because we need an exact match.
+          Remember, I ran this over many files so lots of: "audit.dat", "audit.1783783.dat", and "rev.8371.dat".
      */
 
     for (int i=0; i<parsedLines.size(); i++) {
       if (parsedLines.get(i).startsWith("    select")) {
-        /* match exact start of line because we cannot match randomword.dat
-         matching .dat would be problematic too (what if variable set for file extension)
-         happened: had infileextension = ".dat" outfileextension = ".csv" so lots of bad results
+        /* Match exact start of line because we cannot match "randomword.dat".
+         Matching ".dat" would be problematic too. (What if variable set for file extension?)
+         Happened: had infileextension = ".dat" outfileextension = ".csv", so lots of bad results.
          */
 
         String[] words = parsedLines.get(i).split(" ");
-        // luckily in this case the exact word is the last word in this list
-        // note the substring is to get rid of double quotes
+        // Luckily in this case, the exact word is the last word in this list.
+        // Note: the substring is to get rid of double quotes.
         String datafile = words[words.length-1];
         return datafile.substring(1, datafile.length()-1);
       }
@@ -62,21 +62,21 @@ public class CobolParser {
   }
 
   public String getDataFileRegex() {
-    // So we know that the file is a *.dat file so lets just find that
-    // Compile because that will speed up operations later (we have to run it multiple times)
+    // So we know that the file is a *.dat file so let's just find that.
+    // Compile because that will speed up operations later. (We have to run it multiple times.)
 
     Pattern fileP = Pattern.compile("\\w+.dat");
-    // \w+ means one or more alphanumeric character using \\ because in java "\" is special char
-    Matcher match;  // define early so we don't have to recreate over and over in for loop
+    // \w+ means one or more alphanumeric characters using \\ because in java "\" is a special character.
+    Matcher match;  // Define early so we don't have to recreate over and over in a for loop.
 
     for (String lines : parsedLines) {
-      // So loop through each line in data file and then just match/capture the filename
+      // So loop through each line in data file, then just match/capture the filename.
 
-      // setup our matcher using the earlier compiled pattern
+      // Setup our matcher using the earlier compiled pattern.
       match = fileP.matcher(lines);
       if (match.find()) {
-        // while (match.find()) will loop through all matches - better than contains
-        // because match.find returns an array of groups - we can access the first match with:
+        // match.find() will loop through all matches. It's better than contains
+        // because match.find returns an array of groups. We can access the first match with:
         return match.group(0);
       }
     }
