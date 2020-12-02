36de18073c147fbab24d2f48decea846780e9272
amlopez5841 <74120068+amlopez5841@users.noreply.github.com>
2020-12-01 19:10:17 -0500

    Updated CSVParsers.java
    //Line #19 removed an unused commented out piece of old code.
    //Added comments on lines: #7, #9, #19, #22 
    //Possible spelling error on line #29 ""\tand with regex still: "

--- src/CSVParsers.java
+++ src/CSVParsers.java
@@ -4,19 +4,26 @@ import java.io.IOException;
 
 public class CSVParsers {
   public static void main(String args[]) throws IOException {
+    //Instance call of the object BookCSV.
     BookCSV books = new BookCSV("./data/books.csv");
+    /**
+    * This for each loop places a book that is read by the file reader
+    * into the readCSV method located in the BookCvs method. It also places
+    * two sets of dotted lines with the author located in the center of the lines into a file.
+    */
     for (Book b : books.readCSV()) {
       System.out.println(".......");
       System.out.println(b.getAuthor());
       System.out.println(".......");
     };
+    //After the author is printed, then the book title is displayed as well.
     System.out.println(books.books.get(0).getTitle());
-
+    
+    //Calls the printCSV and writeCSV method.
     books.printCSV();
     books.writeCSV();
 
     CobolParser cp = new CobolParser("legacyHorror.cob");
-    // cp.printCurrentLines();
     System.out.println("data file is: " + cp.getDataFileNoRegex());
     System.out.println("\tand with regex still: " + cp.getDataFileRegex());
 
