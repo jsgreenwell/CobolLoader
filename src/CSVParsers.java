cd04634558e77dddbb0bda59f12a954bb4aee530
Chewy8234 <72936062+Chewy8234@users.noreply.github.com>
2020-12-02 17:29:40 -0500

    Added comments and JavaDocs

--- src/CSVParsers.java
+++ src/CSVParsers.java
@@ -1,7 +1,11 @@
 import edu.loaders.csvparsers.*;
 import edu.loaders.textparsers.CobolParser;
-import java.io.IOException;
-
+import java.io.IOException
+/** This is the  method which makes up the data of the book
+*@param args unused
+*@return Nothing
+*exception IOException on input
+*/
 public class CSVParsers {
   public static void main(String args[]) throws IOException {
     BookCSV books = new BookCSV("./data/books.csv");
@@ -9,8 +13,15 @@ public class CSVParsers {
       System.out.println(".......");
       System.out.println(b.getAuthor());
       System.out.println(".......");
+    /**
+    * Print the author of the book
+    /
     };
+  
     System.out.println(books.books.get(0).getTitle());
+    /**
+    *Prints book and the title
+    /
 
     books.printCSV();
     books.writeCSV();
