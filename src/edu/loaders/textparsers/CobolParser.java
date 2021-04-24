04e2b1df5ea915e50c1ad25ff905635733f0a6bb
tjmacphee <62121474+tjmacphee@users.noreply.github.com>
2021-04-24 19:20:20 -0400

    Update CobolParser.java
    Fixed some grammar issues with spelling, punctuation and pluralization.

--- src/edu/loaders/textparsers/CobolParser.java
+++ src/edu/loaders/textparsers/CobolParser.java
@@ -39,21 +39,21 @@ public class CobolParser {
   public String getDataFileNoRegex() {
     /* old way with no regex
         Problem with this way is what if there are only 2 spaces (you can use \\s+ with regex)
-        What if the lowercase wasn't done (if someone inhereted this and overloaded)
+        What if the lowercase wasn't done? (if someone inherited this and overloaded)
         we cannot use contains because we need an exact match
-          remember I ran this over many files so lots of audit.dat, audit.1783783.dat, rev.8371.dat
+        remember I ran this over many files, so lots of audit.dat, audit.1783783.dat, rev.8371.dat
      */
 
     for (int i=0; i<parsedLines.size(); i++) {
       if (parsedLines.get(i).startsWith("    select")) {
-        /* match exacting start of line because we cannea match randomword.dat
-         matching .dat would be problomatic to (what if variable set for file extinseon)
+        /* match exacting start of line because we cannot match randomword.dat
+         matching .dat would be problematic too (what if variable set for file extinseon)
          happened: had infileextension = ".dat" outfileextension = ".csv" so lots of bad results
          */
 
         String[] words = parsedLines.get(i).split(" ");
-        // luckily in this case the exact word is teh last word in this list
-        // note teh substring is to get rid of double quotes
+        // luckily in this case the exact word is the last word in this list
+        // note the substring is to get rid of double quotes
         String datafile = words[words.length-1];
         return datafile.substring(1, datafile.length()-1);
       }
@@ -62,17 +62,17 @@ public class CobolParser {
   }
 
   public String getDataFileRegex() {
-    // So we know that the file is a *.dat file so lets just find that
-    // Compile because that will speed up operation later (we have to run it multiple time)
+    // So we know that the file is a *.dat file, so lets just find that
+    // Compile because that will speed up operation later (we have to run it multiple times)
 
     Pattern fileP = Pattern.compile("\\w+.dat");
-    // \w+ means one or more alphanumeric character using \\ because in java "\" is special char
+    // \w+ means one or more alphanumeric characters using \\ because in java "\" is a special char
     Matcher match;  // define early so we don't have to recreate over and over in for loop
 
     for (String lines : parsedLines) {
       // So loop through each line in data file and the just match/capture teh filename
 
-      // setup our matcher using teh earlier compiled pattern
+      // setup our matcher using the earlier compiled pattern
       match = fileP.matcher(lines);
       if (match.find()) {
         // while (match.find()) will loop through all matches - better than contains
